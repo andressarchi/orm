6e1a44db8f8236f5d2edbe8fde96a624ea206a1c
andressarchi <andresfelipesarchivalencia@gmail.com>
2024-11-05 08:17:40 -0500

    se hace el proyecto

---
+++ src/main/java/models/Departamento.java
@@ -0,0 +1,64 @@
+package models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "DEPT")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String ubicacion;
+
+    // Relación one-to-many opcional
+    @OneToMany(mappedBy = "departamento")
+    List<Empleado> empleados;
+
+    // Constructor sin parámetros
+    public Departamento() {}
+
+    // Constructor con parámetros
+    public Departamento(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+}
+
